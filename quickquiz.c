249b90834310ae441695a913c7bff512f8340a9d
kumaraman-dev <amankumarnaw23@gmail.com>
2023-10-13 02:04:49 +0530

    hj

---
+++ quickquiz.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void gm();
+void ga();
+void gn();
+int main()
+{
+gm();
+ga();
+gn();
+}
+void gm()
+{
+    printf("\nGood Morning");
+}
+void ga()
+{
+    printf("\nGood Afternoon");
+}
+void gn()
+{
+    printf("\nGood Night");
+}
